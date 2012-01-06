ced3218b984ddcbbe85859d93ed67e503ef853ec
Jon Phipps <jphipps@madcreek.com>
2012-01-06 14:57:52 -0500

    registered foundation namespace

---
+++ autoload.php
@@ -0,0 +1,12 @@
+<?php
+ 
+// remote_test/autoload.php
+ 
+require_once __DIR__.'/vendor/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+ 
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+ 
+$loader = new UniversalClassLoader();
+$loader->register();
+
+$loader->registerNamespace('Symfony\\Component\\HttpFoundation', __DIR__.'/vendor/symfony/src/Symfony/Component/HttpFoundation'); 
