7f326cf0cff9dc93743e015a3622b40354fdc08e
Jon Phipps <jphipps@madcreek.com>
2012-01-10 13:20:25 -0500

    completely revamped vendor dependencies got composer.phar working properly

--- autoload.php
+++ autoload.php
@@ -3,15 +3,9 @@
 // remote_test/autoload.php
  
 require_once __DIR__.'/vendor/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
- 
+
 use Symfony\Component\ClassLoader\UniversalClassLoader;
- 
-$loader = new UniversalClassLoader();
-$loader->registerNamespaces(array(
-    'Symfony'          => __DIR__.'/vendor/symfony/src',
-    'Doctrine\\Common' => __DIR__.'/vendor/doctrine-common/lib',
-    'Doctrine\\DBAL'   => __DIR__.'/vendor/doctrine-dbal/lib',
-    'Doctrine'         => __DIR__.'/vendor/doctrine/lib',
-    'Monolog'          => __DIR__.'/vendor/monolog/src',
-    'Metadata'         => __DIR__.'/vendor/metadata/src',
-));
+
+  $loader = new UniversalClassLoader();
+  $loader->register();
+  $loader->registerNamespace('Symfony\\Component\\HttpFoundation', __DIR__.'/vendor/symfony/http-foundation');
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,16 +1,9 @@
 <?php
 
-/*
- * This file is part of the Silex framework.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+  require_once __DIR__ . '/../vendor/.composer/autoload.php';
 
-if (file_exists($file = __DIR__.'/../autoload.php')) {
-    require_once $file;
-} elseif (file_exists($file = __DIR__.'/../autoload.php.dist')) {
-    require_once $file;
-}
+  use Symfony\Component\HttpFoundation\Request;
+  use Symfony\Component\HttpFoundation\Response;
+
+  $request = Request::create('http://rdvocab');
+
